ae545592deb39a10a3ae22811a7bf1cd2c30e918
Sakin790 <sakinislam79@gmail.com>
2024-03-17 19:22:33 +0600

    might have error

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { healthCheck, registerUser ,loginUser } from "../controllers/user.controller.js";
+import { healthCheck, registerUser ,loginUser ,allUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.route("/healthCheck").get(healthCheck);
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser)
+router.route("/users").get(allUser)
 
 export { router };
